a38787e0e60361d879104c02f6d8122ef5087a0f
d.ruggeroni <d.ruggeroni@lectra.com>
2025-09-24 14:44:08 +0200

    update for meteor 3.3.2

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(["1.0.3.1", "3.0-beta.0"]);
+  api.versionsFrom(["1.0.3.1", "3.0","3.3.2"]);
 
   api.use(["underscore", "mongo", "check", "reactive-var", "modules", "ecmascript"]);
 
